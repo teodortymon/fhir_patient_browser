3a8b9e6d461c5d2a39c42d9d973238cf449b824e
Teodor Wojcik <teodor.wojcik@inera.se>
2023-02-20 16:52:36 +0100

    Add remaining keys

--- config-genrator/generate_config.js
+++ config-genrator/generate_config.js
@@ -35,7 +35,10 @@ function requestPromise(options) {
                 strictSSL: false,
                 proxy    : app.proxy,
                 headers  : {
-                    accept: "application/json+fhir"
+                      accept: "application/json+fhir",
+                "content-type": "application/x-www-form-urlencoded",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoidGVvZG9yd29qY2lrIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiJjM2RkZjM4Yi1mODBhLTQyOWQtODA4YS1mMWQ2NzhlMmVlMDgiLCJqdGkiOiJjNDkzYzYzMi01MjNhLTQ5OGEtYTY4NC0xYmEyZWNjOGViMWIiLCJleHAiOjE2NzY5MTg2MTksImlzcyI6ImFwaS5zaW1wbGlmaWVyLm5ldCIsImF1ZCI6ImFwaS5zaW1wbGlmaWVyLm5ldCJ9.adCz7cIH5nng8Vhgt2jxJQob-TfrO2mPIh7f_ytwWfY",
+                "x-apikey": "LgDHGAHUOcu0i0a3jH5xVEureGRgqiBi9XIazrpjUR6bHERD", 
                 }
             }),
             (error, res) => {
