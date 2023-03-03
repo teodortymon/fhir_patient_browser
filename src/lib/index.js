ffb90fd0c823ce333f5c3d6c1e00adea42a39d41
Teodor Wojcik <teodor.wojcik@inera.se>
2023-03-03 13:29:27 +0100

    Update keys

--- src/lib/index.js
+++ src/lib/index.js
@@ -462,7 +462,7 @@ export function request(options) {
         headers: {
               accept: "application/json+fhir",
                 "content-type": "application/x-www-form-urlencoded",
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoidGVvZG9yd29qY2lrIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiJjM2RkZjM4Yi1mODBhLTQyOWQtODA4YS1mMWQ2NzhlMmVlMDgiLCJqdGkiOiI5ZDU4Mjk3Yy05MjE4LTQ2YmItOGFmYS00MDgzY2M1NTFmN2EiLCJleHAiOjE2NzcwODk4NTYsImlzcyI6ImFwaS5zaW1wbGlmaWVyLm5ldCIsImF1ZCI6ImFwaS5zaW1wbGlmaWVyLm5ldCJ9.eALlgJfLLmff8ZpbMztqu1DG3cj4rqh1ZPY--sdr5tI",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoidGVvZG9yd29qY2lrIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiJjM2RkZjM4Yi1mODBhLTQyOWQtODA4YS1mMWQ2NzhlMmVlMDgiLCJqdGkiOiI3NzYwZTQ2OS1hMjUzLTRkMDYtOTAxZi0wNzMwNTBlZmRkNTMiLCJleHAiOjE2Nzc4NzUwODcsImlzcyI6ImFwaS5zaW1wbGlmaWVyLm5ldCIsImF1ZCI6ImFwaS5zaW1wbGlmaWVyLm5ldCJ9.F_haLggUxOwJgGVIgzBwwKYruQSZRey1E69yVgIKMW4",
                 // "x-apikey": "LgDHGAHUOcu0i0a3jH5xVEureGRgqiBi9XIazrpjUR6bHERD", 
         }
     })
